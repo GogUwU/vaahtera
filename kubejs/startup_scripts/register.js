9f63ece112e46e044fae34571b9ea3bd27c3d4b3
CesiumHorizon <dalick99@gmail.com>
2025-06-13 19:47:19 -0400

    add border points and dev textures
    also updates gitignore to ignore all config

---
+++ kubejs/startup_scripts/register.js
@@ -0,0 +1,6 @@
+StartupEvents.registry('item', e => {
+    e.create('border_point').displayName("§bBorder Point§r").texture('gearsandqueers:item/devborderpoints/borderpoint')
+    e.create('compressed_border_point').displayName("§b§lCompressed Border Point§r").texture('gearsandqueers:item/devborderpoints/borderpointsquare')
+    e.create('super_compressed_border_point').displayName("§b§l§oSuper Compressed Border Point§r").texture('gearsandqueers:item/devborderpoints/superborderpoint')
+    e.create('border_point_singularity').displayName("§k§bA§r§b§l§o<Border Point Singularity>§r§k§bA§r").texture('gearsandqueers:item/devborderpoints/borderpointsingularity')
+})
