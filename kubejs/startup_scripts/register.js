8d401af934f9216ae6f25d131780fe5d062d2448
GogUwU <86622301+GogUwU@users.noreply.github.com>
2025-06-24 16:53:17 +0300

    recipes

--- kubejs/startup_scripts/register.js
+++ kubejs/startup_scripts/register.js
@@ -3,4 +3,6 @@ StartupEvents.registry('item', e => {
     e.create('compressed_border_point').displayName("§b§lCompressed Border Point§r").texture('gearsandqueers:item/devborderpoints/borderpointsquare')
     e.create('super_compressed_border_point').displayName("§b§l§oSuper Compressed Border Point§r").texture('gearsandqueers:item/devborderpoints/superborderpoint')
     e.create('border_point_singularity').displayName("§k§bA§r§b§l§o<Border Point Singularity>§r§k§bA§r").texture('gearsandqueers:item/devborderpoints/borderpointsingularity')
+    e.create('stone_package').displayName("Stone package").texture('gearsandqueers:item/devborderpoints/stone_package')
+    e.create('wood_package').displayName("Wood package").texture('gearsandqueers:item/devborderpoints/wood_package')
 })
