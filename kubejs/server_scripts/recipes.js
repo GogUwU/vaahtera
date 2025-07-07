4319f85f90f82b5c63b19e9d255587d9dae791d5
GogUwU <86622301+GogUwU@users.noreply.github.com>
2025-07-07 22:57:40 +0300

    aaaaaaaaaaaaa

---
+++ kubejs/server_scripts/recipes.js
@@ -0,0 +1,3 @@
+ServerEvents.recipes(e => {
+    e.shapeless('patchouli:guide_book[patchouli:book="advancedperipherals:manual"]', ['minecraft:book', 'computercraft:computer_normal']).id('geersandqueers:book_adv_per')
+})
