ee5dcd3c6649a87717660b6e932becf86abe49c5
GogUwU <86622301+GogUwU@users.noreply.github.com>
2025-07-07 21:19:09 +0300

    no more

--- kubejs/startup_scripts/register.js
+++
@@ -1,8 +0,0 @@
-StartupEvents.registry('item', e => {
-    e.create('border_point').displayName("Border Point").texture('gearsandqueers:item/devborderpoints/borderpoint')
-    e.create('compressed_border_point').displayName("Compressed Border Point").rarity('UNCOMMON').texture('gearsandqueers:item/devborderpoints/borderpointsquare')
-    e.create('super_compressed_border_point').displayName("Super Compressed Border Point").rarity('RARE').texture('gearsandqueers:item/devborderpoints/superborderpoint')
-    e.create('border_point_singularity').displayName("<Border Point Singularity>").rarity('EPIC').texture('gearsandqueers:item/devborderpoints/borderpointsingularity')
-    e.create('stone_package').displayName("Stone package").texture('gearsandqueers:item/devborderpoints/stone_package')
-    e.create('wood_package').displayName("Wood package").texture('gearsandqueers:item/devborderpoints/wood_package')
-})
