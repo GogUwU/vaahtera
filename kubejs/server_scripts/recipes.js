26219adf3b4b5f74865235ff24a1c885fec39228
GogUwU <86622301+GogUwU@users.noreply.github.com>
2025-07-27 07:22:42 +0300

    kubejs stuff

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -1,3 +1,10 @@
 ServerEvents.recipes(e => {
     e.shapeless('patchouli:guide_book[patchouli:book="advancedperipherals:manual"]', ['minecraft:book', 'computercraft:computer_normal']).id('geersandqueers:book_adv_per')
+    e.shapeless('minecraft:chest', ['#c:chests']).id('geersandqueers:chests_alt')
+    e.shaped('nomansland:shelf_mushroom_block', [
+        'AA',
+        'AA'
+    ], {
+        A: 'nomansland:shelf_mushroom'
+    }).id('geersandqueers:shelf_mushroom_block')
 })
---
+++ kubejs/server_scripts/tags.js
@@ -0,0 +1,3 @@
+ServerEvents.tags('item', e => {
+    e.add('c:foods/cooked_egg', 'incubation:fried_egg')
+})
